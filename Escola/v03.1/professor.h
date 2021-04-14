54749fd10c1a0e2d4b669a911426741adc3da1a2
IsaacViana <51500333+IsaacViana@users.noreply.github.com>
2021-04-14 18:15:30 -0300

    Create professor.h

---
+++ Escola/v03.1/professor.h
@@ -0,0 +1,7 @@
+void insereProfessor(struct registro *cadProfessor);
+void listaProfessor(struct registro *cadProfessor);
+int validaSexoProfessor(struct registro *cadProfessor);
+int validaCpfProfessor(struct registro *cadProfessor);
+int gerarMatricula(struct registro *cadProfessor);
+int validaDataNascimento(struct registro *cadProfessor);
+void matricularProfessor(struct matr *cadMatricula);
