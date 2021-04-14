793e0b0b4dd2f04b41d3f0e0833e712260671265
IsaacViana <51500333+IsaacViana@users.noreply.github.com>
2021-04-14 18:14:51 -0300

    Create aluno.h

---
+++ Escola/v03.1/aluno.h
@@ -0,0 +1,7 @@
+void insereAluno(struct registro *cadAluno);
+void listaAluno(struct registro *cadAluno);
+int validaSexoAluno(struct registro *cadAluno);
+int validaCpfAluno(struct registro *cadAluno);
+int gerarMatricula(struct registro *cadAluno);
+int validaDataNascimento(struct registro *cadAluno);
+void matricularAluno(struct matr *cadMatricula);
