4b2ba1c9dec462aec54bc2da19aa2a44a838a573
IsaacViana <51500333+IsaacViana@users.noreply.github.com>
2021-06-03 14:21:12 -0300

    Create mainEscola.c

---
+++ atualEscola/mainEscola.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "novoEscola.h"
+
+int main(){
+
+    printf("----------PROJETO ESCOLA----------\n");
+
+    menu();
+
+    return 0;
+}
